e6c8a71fcdba5f37d2d7d14688b0fb6ef187dacf
maydindev <maydin.dev@outlook.com>
2024-08-11 17:10:41 +0300

    Updated

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -20,9 +20,17 @@ const db = getFirestore(app);
 
 
 // Import the functions you need from the SDKs you need
+/*
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
+*/
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from '@firebase/firestore'
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -39,9 +47,21 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+/*
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
+*/
 
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth();
+const db = getFirestore(app)
 
-export { db };
+// try to add analytics
+const analytics =
+  app.name && typeof window !== 'undefined' ? getAnalytics(app) : null;
+
+export {auth, db, analytics}
+
+//export { db };
