a7d048c1e71751389a973ff45d854d993e9fdf89
maydindev <maydin.dev@outlook.com>
2024-08-11 18:01:17 +0300

    Added export csv file function

--- next.config.mjs
+++ next.config.mjs
@@ -5,40 +5,6 @@ const nextConfig = {
     swcMinify: true,
 };
 
-/*
-module.exports = {
-    reactStrictMode: true,
-  };
-*/
-//module.exports = nextConfig; // export the config as a module
-
-// next.config.js
-/*
-module.exports = {
-    reactStrictMode: true,
-    swcMinify: true,
-  };
-*/
-
-
-
-//export default nextConfig;
-
-/*
-import { nextConfig } from 'next';
-
-export default nextConfig({
-  // Konfigürasyon ayarlarınız buraya gelir
-  reactStrictMode: true,
-  ssr: false,
-  swcMinify: true,
-  webpack(config, { buildId, dev, isServer, defaultLoaders, webpack }) {
-    config.resolve.extensions.push('.js', '.jsx', '.json');
-    return config;
-  },
-});
-*/
-
 export default {
   webpack: (config, { isServer }) => {
     if (!isServer) {
--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,41 +1,10 @@
-// firebase.js
-/*
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+//https://stackoverflow.com/questions/69799682/firebase-analytics-with-next-js-window-not-defined
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-*/
-
-
-
-
-// Import the functions you need from the SDKs you need
-/*
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore';
-*/
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from '@firebase/firestore'
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAR3ZyHsRD_3JymbRTn41-sDnGzvzXUB5g",
   authDomain: "crud-firebase-1c0dd.firebaseapp.com",
@@ -46,22 +15,11 @@ const firebaseConfig = {
   measurementId: "G-VC3EY3FRTX"
 };
 
-// Initialize Firebase
-/*
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
-*/
-
-// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth();
 const db = getFirestore(app)
 
-// try to add analytics
 const analytics =
   app.name && typeof window !== 'undefined' ? getAnalytics(app) : null;
 
 export {auth, db, analytics}
-
-//export { db };
